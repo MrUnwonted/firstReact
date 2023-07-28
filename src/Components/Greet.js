3113581906bd001e9d0eeeffdee4d42e609b1e51
MrUnwonted <arjungodzarjun@gmail.com>
2023-07-29 01:10:05 +0530

    First Commit

---
+++ src/Components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello World</h1>
+// }
+
+// Using Arrow Function we can create like this
+// This is named export
+export default () => <h1>Functional Component</h1>
+
+// - This is default export
+// export default Greet
